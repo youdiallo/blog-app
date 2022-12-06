dde7d73f1af48f4efac0694eadbacc717896b5dc
Youssouf DIALLO <youssouf.diallo3195@gmail.com>
2022-12-06 10:57:38 +0100

    Adding main navigation section (to all pages)

---
+++ components/home-page/futured-posts.js
@@ -0,0 +1,5 @@
+function FuturedPosts() {
+
+}
+
+export default FuturedPosts;
---
+++ components/layout/logo.js
@@ -0,0 +1,8 @@
+import classes from './logo.module.css';
+
+function Logo() {
+    return <div className={classes.logo}>
+        Youssouf' Blog
+    </div>
+}
+export default Logo;
