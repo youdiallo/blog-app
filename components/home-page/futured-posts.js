23415a20f37e0a1e2cad4c707612cac627d369c5
Youssouf DIALLO <youssouf.diallo3195@gmail.com>
2022-12-06 12:04:43 +0100

    Adding Futured section to Home page (with dummy data)

--- components/home-page/futured-posts.js
+++ components/home-page/futured-posts.js
@@ -1,5 +1,13 @@
-function FuturedPosts() {
+import PostGrid from "../posts/post-grid";
+import classes from './futured-posts.module.css';
 
+function FuturedPosts(props) {
+    return (
+        <section className={classes.latest}>
+            <h2>Featured Posts</h2>
+            <PostGrid posts={props.posts} />
+        </section>
+    );
 }
 
 export default FuturedPosts;
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,42 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FuturedPosts from '../components/home-page/futured-posts';
 
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started With NexJS', 
+    image: 'getting-started-nextjs.png', 
+    slug: 'getting-started-with-nextjs', 
+    date: '2022-02-23', 
+    excerpt: 'NextJS is the react framework for production - it makes building fullstack React app easier with SSR.'
+  },
+  {
+    title: 'Getting Started With NexJS', 
+    image: 'getting-started-nextjs.png', 
+    slug: 'getting-started-with-nextjs1', 
+    date: '2022-02-23', 
+    excerpt: 'NextJS is the react framework for production - it makes building fullstack React app easier with SSR.'
+  },
+  {
+    title: 'Getting Started With NexJS', 
+    image: 'getting-started-nextjs.png', 
+    slug: 'getting-started-with-nextjs2', 
+    date: '2022-02-23', 
+    excerpt: 'NextJS is the react framework for production - it makes building fullstack React app easier with SSR.'
+  },
+  {
+    title: 'Getting Started With NexJS', 
+    image: 'getting-started-nextjs.png', 
+    slug: 'getting-started-with-nextjs3', 
+    date: '2022-02-23', 
+    excerpt: 'NextJS is the react framework for production - it makes building fullstack React app easier with SSR.'
+  }
+];
+
 export default function HomePage() {
   return (
     <Fragment >
       <Hero />
-      <FuturedPosts />
+      <FuturedPosts posts={DUMMY_POSTS}/>
     </Fragment>
   )
 }
