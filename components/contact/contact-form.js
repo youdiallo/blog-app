4146908613a39a3b712c4ec55b67f226a765cfca
Youssouf DIALLO <40734213+youdiallo@users.noreply.github.com>
2022-12-07 21:13:40 +0100

    Adding UI Feedback with notification

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,5 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import classes from './contact-form.module.css';
+import Notification from '../../ui/notification';
+
+async function sendContactData(contactData) {
+    const response =  await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(contactData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const rspData = await response.json();
+
+    if(!response.ok) {
+        throw new Error(rspData.message || 'Something went wrong!');
+    }
+}
 
 function ContactForm() {
 
@@ -7,56 +23,109 @@ function ContactForm() {
     const emailRef = useRef();
     const messageRef = useRef();
 
-    function formSubmitHandler(event) {
+    const [ requestStatus, setResquestStatus] = useState(); //'pendding', 'success', or 'error'
+    const [ requestError, setResquestError ] = useState();
+
+    useEffect(() => {
+
+        if(requestStatus === 'success' || requestStatus === 'error') {
+            const timer = setTimeout(() => {
+                setResquestStatus(null);
+                setResquestError(null);
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+
+    }, [requestStatus])
+
+    async function formSubmitHandler(event) {
         event.preventDefault();
 
         const enteredEmail = emailRef.current.value;
         const enteredName = nameRef.current.value;
         const enteredMessage = messageRef.current.value;
         
-        //Optional input validation
-        fetch('/api/contact', {
-            method: 'POST',
-            body: JSON.stringify({
+        setResquestStatus('pending');
+        try {
+            await sendContactData({
                 email: enteredEmail,
                 name: enteredName,
                 message: enteredMessage
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        });
+            });
+            setResquestStatus('success');
+            emailRef.current.value = '';
+            nameRef.current.value = '';
+            messageRef.current.value = '';
+        } catch (error) {
+            setResquestError(error.message);
+            setResquestStatus('error');
+        } 
+    }
+
+    let notification;
+
+    if(requestStatus === 'pending') {
+        notification = {
+            status: 'pending',
+            title: 'Sending message...',
+            message: 'Your message is on its way!'
+        };
+    }
+
+    if(requestStatus === 'success') {
+        notification = {
+            status: 'success',
+            title: 'Success!',
+            message: 'Message sent successfuly'
+        };
+    }
+
+    if(requestStatus === 'error') {
+        notification = {
+            status: 'error',
+            title: 'Error!',
+            message: requestError
+        };
     }
 
     return (
-        <section className={classes.contact}>
-            <h1>How can I help you?</h1>
-            <form className={classes.form} onSubmit={formSubmitHandler}>
-                <div className={classes.controls}>
-                    <div className={classes.control}>
-                        <label htmlFor='email'>Your Email</label>
-                        <input type='email' id='email' required ref={emailRef}/>
-                    </div>
-                    <div className={classes.control}>
-                        <label htmlFor='name'>Your Name</label>
-                        <input type='text' id='name' required ref={nameRef}/>
-                    </div>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='message'>Your Message</label>
-                    <textarea id='message' rows='5' required ref={messageRef}></textarea>
-                </div>
+      <section className={classes.contact}>
+        <h1>How can I help you?</h1>
+        <form className={classes.form} onSubmit={formSubmitHandler}>
+          <div className={classes.controls}>
+            <div className={classes.control}>
+              <label htmlFor="email">Your Email</label>
+              <input type="email" id="email" required ref={emailRef} />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="name">Your Name</label>
+              <input type="text" id="name" required ref={nameRef} />
+            </div>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="message">Your Message</label>
+            <textarea
+              id="message"
+              rows="5"
+              required
+              ref={messageRef}
+            ></textarea>
+          </div>
 
-                <div className={classes.actions}>
-                    <button>Send Message</button>
-                </div>
-            </form>
+          <div className={classes.actions}>
+            <button>Send Message</button>
+          </div>
+        </form>
 
-        </section>
+        {notification && (
+          <Notification
+            status={notification.status}
+            title={notification.title}
+            message={notification.message}
+          />
+        )}
+      </section>
     );
 }
 
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient }  from 'mongodb';
+ 
+async function handler(req, res) {
     if(req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -24,9 +26,29 @@ function handler(req, res) {
         };
         console.log(newMessage);
 
-        res
-        .status(201)
-        .json({
+        let mongoClient;
+        try {
+            mongoClient = await MongoClient.connect('mongodb+srv://youdiallo:zaUTehkFu5lnb5HO@cluster0.db97nxc.mongodb.net/?retryWrites=true&w=majority');
+        } catch(error){
+            console.log(error);
+            res.status(500).json({
+                message: 'Can not connect to the database!'
+            });
+            return;
+        }
+        const db = mongoClient.db('blog-app');
+        try {
+            const result = db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Can insert data to the database!'
+            });
+            return;
+        }
+
+        res.status(201).json({
             sentmessage: newMessage
         })
 
