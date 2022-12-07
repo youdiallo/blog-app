62afebd014587f731bcb0a60f7e99c64f6de8416
Youssouf DIALLO <40734213+youdiallo@users.noreply.github.com>
2022-12-07 12:37:27 +0100

    Add syntax highliting for code Snippets.

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from "./post-header";
 import classes from './post-content.module.css';
@@ -9,23 +11,33 @@ function PostContent(props) {
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     const customRenderers = {
-        p(paragraph) {
-            const { node } = paragraph;
-            if(node.children[0].tagName === 'img') {
-                const image = node.children[0];
-                return (
-                    <div className={classes.image}>
-                      < Image
-                        src={`/images/posts/${post.slug}/${image.properties.src}`}
-                        alt={image.properties.alt}
-                        width={600}
-                        height={300}
-                      />
-                    </div>
-                  );
-            }
-            return <p>{paragraph.children}</p>;
+      p(paragraph) {
+        const { node } = paragraph;
+        if (node.children[0].tagName === "img") {
+          const image = node.children[0];
+          return (
+            <div className={classes.image}>
+              <Image
+                src={`/images/posts/${post.slug}/${image.properties.src}`}
+                alt={image.properties.alt}
+                width={600}
+                height={300}
+              />
+            </div>
+          );
         }
+        return <p>{paragraph.children}</p>;
+      },
+
+      code(code) {
+        const _array = code.className.split("-");
+        const _language = _array[1];
+        return (
+          <SyntaxHighlighter language={_language} style={atomDark}>
+            {code.children[0]}
+          </SyntaxHighlighter>
+        );
+      },
     };
 
     return <article className={classes.content}>
